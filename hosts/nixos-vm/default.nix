6d7fb5681176cc1e95d675152b745b880cae5d3b
Alexander Zerpa <57498118+alexander-zerpa@users.noreply.github.com>
2024-09-04 12:16:35 +0000

    fixed efi errors

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -13,7 +13,7 @@
 
   # Bootloader.
   # boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
 
