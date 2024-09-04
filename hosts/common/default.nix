2ccd87df76ad07a1bafebe1c97ace06dfec6242f
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2024-09-04 15:10:17 +0200

    change host key gen

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -22,6 +22,7 @@
     defaultSopsFile = ../../secrets.yaml;
 
     age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
