39b530c1077b98401a1f684e09c5bd57afffeff9
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2024-09-05 03:05:33 +0200

    added zaleos to flake

--- flake.nix
+++ flake.nix
@@ -19,6 +19,14 @@
           ./hosts/nixos-vm/disko.nix
         ];
       };
+      nixos-vm = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./hosts/zaleos
+          sops-nix.nixosModules.sops
+          disko.nixosModules.disko
+          ./hosts/zaleos/disko.nix
+        ];
+      };
     };
 
     packages.x86_64-linux.disko = disko.packages.x86_64-linux.disko;
