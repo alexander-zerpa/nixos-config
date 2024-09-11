9ee49683adb6d0f84a356e4ede1c73e3272c3a4c
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2024-09-11 19:35:08 +0200

    added bluetooth

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -80,6 +80,11 @@
     variant = "";
   };
 
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  services.blueman.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
