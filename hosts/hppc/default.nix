a0ae82f477e416d1ac02dbefad935876b688bb3f
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2024-11-14 20:16:24 +0100

    feat(hosts.hppc): add `steam` with `proton-ge`

--- hosts/hppc/default.nix
+++ hosts/hppc/default.nix
@@ -67,6 +67,13 @@
     polkit_gnome
   ];
 
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
+
   hardware.opengl = {
     enable = true;
     driSupport = true;
