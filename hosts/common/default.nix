fe30b0f171ef6daff603eec686d4eb158ab98605
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2026-01-16 18:50:13 +0100

    feat(hosts.common): clean boot with plymouth

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -17,6 +17,18 @@
     ];
   };
 
+  boot.consoleLogLevel = 3;
+  boot.initrd.systemd.enable = true;
+  boot.initrd.verbose = false;
+  boot.plymouth.enable = true;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "udev.log_priority=3"
+    "rd.systemd.show_status=auto"
+  ];
+
   services = {
     displayManager = {
       ly = {
