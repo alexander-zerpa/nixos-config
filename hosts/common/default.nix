dda81f10fd1c333a4c96abb8d218c7c312f4166a
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2026-07-26 01:50:17 +0200

    feat(hosts.common): use NerdFont font for serif and sans-serif

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -241,18 +241,25 @@
     packages = with pkgs; [
       nerd-fonts.jetbrains-mono
       nerd-fonts.dejavu-sans-mono
+      nerd-fonts.symbols-only
       dejavu_fonts
       unifont
       noto-fonts-color-emoji
     ];
     fontconfig = {
       defaultFonts = {
-        serif = [ "DejaVu Serif" ];
-        sansSerif = [ "DejaVu Sans" ];
-        monospace = [ "JetBrainsMono Nerd Font" "DejaVuSansM Nerd Font" "DejaVu Sans Mono" "Noto Color Emoji" ];
-        emoji = [ "Noto Color Emoji" ] ;
+        serif = [ "DejaVuSansM Nerd Font" ];
+        sansSerif = [ "DejaVuSansM Nerd Font" ];
+        monospace = [
+          "JetBrainsMono Nerd Font"
+          "DejaVuSansM Nerd Font"
+          "DejaVu Sans Mono"
+          "Noto Color Emoji"
+        ];
+        emoji = [ "Noto Color Emoji" ];
       };
     };
+    fontDir.enable = true;
   };
 
   environment.sessionVariables = {
